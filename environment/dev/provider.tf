5b125316ed8db5e2f17a1c1ba1ae395e1a365f02
vipulsingh463 <112720018+vipulsingh463@users.noreply.github.com>
2026-05-20 11:34:31 +0530

    Update provider.tf

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -2,12 +2,12 @@ terraform {
     required_providers {
       azurerm = {
         source = "hashicorp/azurerm"
-        version = "4.59.0"
+        version = "4.73.0"
       }
     }
 }
 
 provider "azurerm" {
     features {}
-    subscription_id = "e9353bd8-5b79-4a91-9500-36beb348477e"
+    subscription_id = "1e10c906-b475-44f5-8e2a-ba4f0d6d8803"
 }
